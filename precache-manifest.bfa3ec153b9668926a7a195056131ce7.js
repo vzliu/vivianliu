748958701cb2774a034f951ff0d17326b4cc247d
Vivian Liu <vivizliu@berkeley.edu>
2024-11-04 00:18:50 -0800

    Updates

---
+++ precache-manifest.bfa3ec153b9668926a7a195056131ce7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cc308180757badf2011f1c4b7c09fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72a04585a4244108ab30",
+    "url": "./static/css/2.6f85094a.chunk.css"
+  },
+  {
+    "revision": "a74bfbd130626d89cd7b",
+    "url": "./static/css/main.4b07c690.chunk.css"
+  },
+  {
+    "revision": "72a04585a4244108ab30",
+    "url": "./static/js/2.35628048.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "./static/js/2.35628048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74bfbd130626d89cd7b",
+    "url": "./static/js/main.b0bd670a.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  }
+]);
